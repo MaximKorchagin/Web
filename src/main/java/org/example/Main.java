d7832b406427ba09dbd3ce0cef45f6e4cb011f05
Maxim.Korchagin <Airfallsc2@gmail.com>
2023-12-04 08:32:39 +0300

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        final var validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+        Server server = new Server(8888, validPaths);
+        server.start();
+    }
+}
